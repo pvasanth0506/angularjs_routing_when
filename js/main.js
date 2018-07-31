b69631843d90d26b4a3fdec0f837d5ff47218380
pvasanth0506 <vasanthakumar0506@gmail.com>
2018-07-31 08:57:03 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const app = angular.module('myApp', ['ngRoute']);
+
+app.config(($routeProvider) => {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../assets/home.html'
+        })
+        .when('/about', {
+            templateUrl: '../assets/about.html'
+        })
+        .when('/contact', {
+            templateUrl: '../assets/contact.html'
+        });
+});
